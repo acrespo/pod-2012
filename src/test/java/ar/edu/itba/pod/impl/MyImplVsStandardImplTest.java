a495b64a18e16a03850a13debe259df75e7ddcab
Cristian Pereyra <criis.pereyra@gmail.com>
2012-10-29 16:47:02 -0300

    distributed side by side test

--- src/test/java/ar/edu/itba/pod/impl/MyImplVsStandardImplTest.java
+++ src/test/java/ar/edu/itba/pod/impl/MyImplVsStandardImplTest.java
@@ -1,7 +1,17 @@
 package ar.edu.itba.pod.impl;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.jgroups.ChannelListener;
+
 import ar.edu.itba.pod.api.SignalProcessor;
+import ar.edu.itba.pod.legajo51190.impl.CompositeTestableSignalNode;
 import ar.edu.itba.pod.legajo51190.impl.MultiThreadedDistributedSignalProcessor;
+import ar.edu.itba.pod.legajo51190.impl.NodeTestController;
+import ar.edu.itba.pod.legajo51190.impl.SignalNode;
+import ar.edu.itba.pod.legajo51190.impl.SignalNodeTestFactory;
+import ar.edu.itba.pod.legajo51190.impl.SyncListener;
 
 /**
  * Side by side test without rmi for inspecting the performance of the internal
@@ -11,18 +21,32 @@ import ar.edu.itba.pod.legajo51190.impl.MultiThreadedDistributedSignalProcessor;
  */
 public class MyImplVsStandardImplTest extends SideBySideTester {
 
-	private static SignalProcessor sp;
+	private static NodeTestController controller;
 
 	@Override
 	protected SignalProcessor init() throws Exception {
-		if (sp == null) {
-			System.out.println("Using "
-					+ Runtime.getRuntime().availableProcessors()
-					+ " processors");
-			sp = new MultiThreadedDistributedSignalProcessor(Runtime.getRuntime()
-					.availableProcessors());
+		if (controller == null) {
+			controller = new NodeTestController(new SignalNodeTestFactory() {
+				@Override
+				public SignalNode getNewSignalNode(final SyncListener listener) {
+					try {
+						Set<ChannelListener> listeners = new HashSet<>();
+						listeners.add(listener);
+						return new CompositeTestableSignalNode(
+								new MultiThreadedDistributedSignalProcessor(1,
+										listeners, listener), listener);
+					} catch (Exception e) {
+						e.printStackTrace();
+						return null;
+					}
+				}
+			});
+			controller.addNewNodes(5);
+		} else {
+			controller.disconnectAllNodesFromChannel();
+			controller.getNodesToTest().clear();
+			controller.addNewNodes(5);
 		}
-		((MultiThreadedDistributedSignalProcessor) sp).exit();
-		return sp;
+		return controller.getNodesToTest().getFirst();
 	}
 }
